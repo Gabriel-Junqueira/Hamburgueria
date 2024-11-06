09cc8aa02b8198a6f73006135f8fc58698b47d65
Gabriel-Junqueira <131571944+Gabriel-Junqueira@users.noreply.github.com>
2024-11-05 22:16:35 -0300

    Add files via upload

---
+++ site.js
@@ -0,0 +1,11 @@
+﻿function mostrarMensagemSucesso(mensagem) {
+    alert(mensagem);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    var mensagem = document.getElementById("mensagem").textContent.trim();
+    
+    if (mensagem) {
+        mostrarMensagemSucesso(mensagem);
+    }
+});
